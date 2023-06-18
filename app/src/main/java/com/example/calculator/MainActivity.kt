6fc4ee8b4ce32388a8dddbfef8fa5e7081d5ba0a
abushka110 <qyjodixilu@gmail.com>
2023-06-19 00:31:14 +0300

    setOnClick for buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.calculator
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -19,13 +20,68 @@ class MainActivity : AppCompatActivity() {
         val btnNumSeven = findViewById<Button>(R.id.btnSeven)
         val btnNumEight = findViewById<Button>(R.id.btnEight)
         val btnNumNine = findViewById<Button>(R.id.btnNine)
+
         val btnDivision = findViewById<Button>(R.id.btnDivision)
         val btnMultiplication = findViewById<Button>(R.id.btnmultiplication)
         val btnSubtraction = findViewById<Button>(R.id.btnSubtraction)
         val btnAddition = findViewById<Button>(R.id.btnAddition)
         val btnEquation = findViewById<Button>(R.id.btnEquation)
         val btnClear = findViewById<Button>(R.id.btnClear)
+
+        btnNumZero.setOnClickListener {
+            onDigit()
+        }
+        btnNumOne.setOnClickListener {
+            onDigit()
+        }
+        btnNumTwo.setOnClickListener {
+            onDigit()
+        }
+        btnNumThree.setOnClickListener {
+            onDigit()
+        }
+        btnNumFour.setOnClickListener {
+            onDigit()
+        }
+        btnNumFive.setOnClickListener {
+            onDigit()
+        }
+        btnNumSix.setOnClickListener {
+            onDigit()
+        }
+        btnNumSeven.setOnClickListener {
+            onDigit()
+        }
+        btnNumEight.setOnClickListener {
+            onDigit()
+        }
+        btnNumNine.setOnClickListener {
+            onDigit()
+        }
+
+        btnDivision.setOnClickListener {
+            onDigit()
+        }
+        btnMultiplication.setOnClickListener {
+            onDigit()
+        }
+        btnSubtraction.setOnClickListener {
+            onDigit()
+        }
+        btnAddition.setOnClickListener {
+            onDigit()
+        }
+        btnEquation.setOnClickListener {
+            onDigit()
+        }
+        btnClear.setOnClickListener {
+            onDigit()
+        }
+
+
     }
 
-
+    private fun onDigit() {
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+    }
 }
