4f70d7d388bac1154247fb7997ce6abf979f0e6b
abushka110 <qyjodixilu@gmail.com>
2023-06-21 15:05:08 +0300

    functions for numbers and clear

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.calculator
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -14,79 +14,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         tvInput = findViewById(R.id.tvInput)
-
-        val btnNumZero = findViewById<Button>(R.id.btnZero)
-        val btnNumOne = findViewById<Button>(R.id.btnOne)
-        val btnNumTwo = findViewById<Button>(R.id.btnTwo)
-        val btnNumThree = findViewById<Button>(R.id.btnThree)
-        val btnNumFour = findViewById<Button>(R.id.btnFour)
-        val btnNumFive = findViewById<Button>(R.id.btnFive)
-        val btnNumSix = findViewById<Button>(R.id.btnSix)
-        val btnNumSeven = findViewById<Button>(R.id.btnSeven)
-        val btnNumEight = findViewById<Button>(R.id.btnEight)
-        val btnNumNine = findViewById<Button>(R.id.btnNine)
-
-        val btnDivision = findViewById<Button>(R.id.btnDivision)
-        val btnMultiplication = findViewById<Button>(R.id.btnmultiplication)
-        val btnSubtraction = findViewById<Button>(R.id.btnSubtraction)
-        val btnAddition = findViewById<Button>(R.id.btnAddition)
-        val btnEquation = findViewById<Button>(R.id.btnEquation)
-        val btnClear = findViewById<Button>(R.id.btnClear)
-
-        btnNumZero.setOnClickListener {
-            onDigit()
-        }
-        btnNumOne.setOnClickListener {
-            onDigit()
-        }
-        btnNumTwo.setOnClickListener {
-            onDigit()
-        }
-        btnNumThree.setOnClickListener {
-            onDigit()
-        }
-        btnNumFour.setOnClickListener {
-            onDigit()
-        }
-        btnNumFive.setOnClickListener {
-            onDigit()
-        }
-        btnNumSix.setOnClickListener {
-            onDigit()
-        }
-        btnNumSeven.setOnClickListener {
-            onDigit()
-        }
-        btnNumEight.setOnClickListener {
-            onDigit()
-        }
-        btnNumNine.setOnClickListener {
-            onDigit()
-        }
-
-        btnDivision.setOnClickListener {
-            onDigit()
-        }
-        btnMultiplication.setOnClickListener {
-            onDigit()
-        }
-        btnSubtraction.setOnClickListener {
-            onDigit()
-        }
-        btnAddition.setOnClickListener {
-            onDigit()
-        }
-        btnEquation.setOnClickListener {
-            onDigit()
-        }
-        btnClear.setOnClickListener {
-            onDigit()
-        }
-
-
     }
 
-    private fun onDigit() {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
+    }
+
+    fun onClear (view: View) {
+        tvInput?.text = "0"
     }
 }
